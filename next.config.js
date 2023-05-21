9bb64a30e890e55551aab04b87ac58ced9292143
TobiasViskum <tobias.t.viskum@gmail.com>
2023-05-21 10:30:30 +0200

    new commit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPWA = require("next-pwa")({
   dest: "public",
   dynamicStartUrl: (req) => {
     const uid = req.query.uid;
-    return `/?uid=${uid}`;
+    return `/?uid=d5141240-33e2-4d7a-807e-e5df34b64d71`;
   },
 });
 
