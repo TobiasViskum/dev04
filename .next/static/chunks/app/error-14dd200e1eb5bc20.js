5d43bc956cd4110d9dc4420e41494405a032293d
TobiasViskum <tobias.t.viskum@gmail.com>
2023-05-21 10:23:06 +0200

    new again

---
+++ .next/static/chunks/app/error-14dd200e1eb5bc20.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[601],{1765:function(e,n,r){Promise.resolve().then(r.bind(r,6310))},6310:function(e,n,r){"use strict";r.r(n),r.d(n,{default:function(){return i}});var t=r(3955),o=r(2310);function i(e){let{error:n,reset:r}=e;return(0,o.useEffect)(()=>{console.error(n)},[n]),(0,t.jsxs)("div",{style:{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"},children:[(0,t.jsx)("h2",{children:"Something went wrong!"}),(0,t.jsx)("button",{onClick:()=>r(),children:"Try again"})]})}},3009:function(e,n,r){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=r(2310),o=Symbol.for("react.element"),i=Symbol.for("react.fragment"),s=Object.prototype.hasOwnProperty,c=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function u(e,n,r){var t,i={},u=null,l=null;for(t in void 0!==r&&(u=""+r),void 0!==n.key&&(u=""+n.key),void 0!==n.ref&&(l=n.ref),n)s.call(n,t)&&!f.hasOwnProperty(t)&&(i[t]=n[t]);if(e&&e.defaultProps)for(t in n=e.defaultProps)void 0===i[t]&&(i[t]=n[t]);return{$$typeof:o,type:e,key:u,ref:l,props:i,_owner:c.current}}n.Fragment=i,n.jsx=u,n.jsxs=u},3955:function(e,n,r){"use strict";e.exports=r(3009)}},function(e){e.O(0,[875,474,744],function(){return e(e.s=1765)}),_N_E=e.O()}]);
