fc1bac36e5e067ab06dcc4ae290ff91dc9e1d3ee
TobiasViskum <tobias.t.viskum@gmail.com>
2023-05-21 10:33:38 +0200

    new

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 const withPWA = require("next-pwa")({
   dest: "public",
-  dynamicStartUrl: (req) => {
-    const uid = req.query.uid;
-    return `/?uid=d5141240-33e2-4d7a-807e-e5df34b64d71`;
-  },
+  // dynamicStartUrl: (req) => {
+  //   const uid = req.query.uid;
+  //   return `/?uid=d5141240-33e2-4d7a-807e-e5df34b64d71`;
+  // },
 });
 
 /** @type {import('next').NextConfig} */
