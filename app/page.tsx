418d10dda8a812f4d2feab8b40423adb78505c7d
TobiasViskum <tobias.t.viskum@gmail.com>
2023-05-22 21:54:33 +0200

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.scss";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Viskum App",
@@ -7,8 +8,23 @@ export const metadata: Metadata = {
 
 export default async function page() {
   return (
-    <>
+    <div className={styles.main}>
+      <br />
       <h1>Viskum App</h1>
-    </>
+      <br />
+      <Link href={"/login"}>Go to Login Page</Link>
+      <br />
+      <br />
+      <h2>You can download this website as an App! (PWA)</h2>
+      <p>But it{"'"}s recommended to wait until you have logged in</p>
+      <br />
+      <br />
+      <h3>How to download:</h3>
+      <h4>iOS:</h4>
+      <p>Share {"→"} Add to Homescreen</p>
+      <br />
+      <h4>Android:</h4>
+      <p>...</p>
+    </div>
   );
 }
